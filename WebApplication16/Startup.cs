91281a13af518b3ffa16a1b6d2eb63b33faea5c0
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-14 16:36:50 +0700

    1234

---
+++ WebApplication16/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication16.Startup))]
+namespace WebApplication16
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
